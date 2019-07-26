5e33e3170a5d49b7ede1d3f496e6a7ffae418a86
Imanirakarama Alexianne <imanirax@yahoo.fr>
2019-07-26 18:41:20 +0200

    styled the portfolio

--- js/script.js
+++ js/script.js
@@ -14,17 +14,50 @@ $(document).ready(function() {
     });
 });
 
+$("button").click(function(party) {
+    var client = document.getElementById('contact-name').value;
+    alert('Thanks ' + ' we have received your message successful. ' + '!');
+    party.preventDefault();
+});
+
+// var btn = document.getElementById("myBtn");
+
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function(event) {
+//     // Validate form for entry
+//     var x = document.forms["myForm"]["contact-name"].value;
+//     if (x == "") {
+//         alert("Name not entered");
+//         return false;
+//     }
+//     var y = document.forms["myForm"]["contact-email"].value;
+//     if (y == "") {
+//         alert("Email not entered");
+//         return false;
+//     }
+//     var z = document.forms["myForm"]["contact-message"].value;
+//     if (z == "") {
+//         alert("You have no message to send");
+//         return false;
+//     }
+//     var person1Input = $("input#contact-name").val();
+//     $("span#outputName").append(person1Input);
+
+//     $('.toast').toast('show');
+//     event.preventDefault();
+// }
+
 // $(document).ready(function() {
 //     $("div.image-frame").hover(function() {
-//         $(".classAdd1").addClass("mask flex-center rgba-white-slight");
+//         $(".classAdd1").hover("mask flex-center rgba-red-strong");
 //     });
 //     $("div.image-frame").hover(function() {
-//         $(".classAdd2").addClass("mask flex-center rgba-white-slight");
+//         $(".classAdd2").addClass("mask flex-center rgba-blue-light");
 //     });
 //     $("div.image-frame").hover(function() {
-//         $(".classAdd3").addClass("mask flex-center rgba-white-slight");
+//         $(".classAdd3").addClass("mask flex-center rgba-green-slight");
 //     });
 //     $("div.image-frame").hover(function() {
-//         $(".classAdd4").addClass("mask flex-center rgba-white-slight");
+//         $(".classAdd4").addClass("mask flex-center");
 //     });
 // });
